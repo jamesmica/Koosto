80705e3d6b13ce3c8e01cf846ececf7403359173
jamesmica <123773695+jamesmica@users.noreply.github.com>
2024-04-11 15:27:23 +0200

    test koosto

--- script.js
+++ script.js
@@ -1,158 +1,200 @@
+var data;
+var currentIsochrone;
+let codesINSEE = new Set();
+console.log(data);
+window.addEventListener("message", function(event) {
+    if (event.origin != 'http://koosto.fr' && event.origin != 'http://editor.weweb.io' && event.origin != 'https://editor.weweb.io' && event.origin != 'https://koosto.fr' && event.origin != 'https://www.koosto.fr') {
+        alert('origine inconnue : ', event.origin);
+        return;
+    }
+        
+    alert( "received: " + event.data );
+    var data = JSON.parse(event.data);
+
+    fetchIsochrone(carte, data);
+
+    });
+
+
+    var lat = data.lat;
+    var lon = data.lon;
+
 function initialiserCarte() {
-    var carte = L.map('maCarte').setView([48.8566, 2.3522], 13);
+    var carte = L.map('maCarte').setView([lat, lon], 13);
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
         attribution: '© OpenStreetMap contributors'
     }).addTo(carte);
     return carte;
 }
-
-var currentIsochrone = null;
 var carte = initialiserCarte();
-
 function fetchIsochrone(map, center) {
+    // console.log('fetchIsochrone(map, center)');
     var apiKey = 'pk.eyJ1IjoiamFtZXNpdGhlYSIsImEiOiJjbG93b2FiaXEwMnVpMmpxYWYzYjBvOTVuIn0.G2rAo0xl14oye9YVz4eBcw';
-    var minutes = 10; // Durée en minutes pour l'isochrone
+    var url = `https://api.mapbox.com/isochrone/v1/mapbox/${center.mode}/${center.lon},${center.lat}?contours_minutes=${center.time || 10}&polygons=true&access_token=${apiKey}`;
 
-    var url = `https://api.mapbox.com/isochrone/v1/mapbox/${center.mode}/${center.lon},${center.lat}?contours_minutes=${center.time || minutes}&polygons=true&access_token=${apiKey}`;
-    var isochronePolygon = null;
-    fetch(url)
-    .then(response => response.json())
-    .then(data => {
-        // Le premier feature contient les coordonnées de l'isochrone
-        if (currentIsochrone) {
-            currentIsochrone.remove();
+    // Retourner une promesse qui se résout une fois que l'isochrone est chargé
+    return new Promise((resolve, reject) => {
+        fetch(url)
+        .then(response => response.json())
+        .then(data => {
+            if (currentIsochrone) {
+                currentIsochrone.remove();
+            }
+            var coords = data.features[0].geometry.coordinates[0];
+            var latLngs = coords.map(coord => ([coord[1], coord[0]]));
+
+            currentIsochrone = L.polygon(latLngs, { color: '#FF0000', weight: 2, opacity: 0.8, fillColor: '#ffffff', fillOpacity: 0.01 }).addTo(map);
+            // console.log(currentIsochrone); 
+            map.setView([center.lat, center.lon], 13);
+            resolve(currentIsochrone); // Résoudre la promesse avec l'isochrone
+        })
+        .catch(error => {
+            console.log('Erreur lors de la récupération des isochrones :', error);
+            reject(error); // Rejeter la promesse en cas d'erreur
+        });
+    });
+}
+
+
+function creerGrillePointsEtAfficherSurCarte(isochrone, pas, map) {
+    // console.log('creerGrillePointsEtAfficherSurCarte(isochrone, pas, map)');
+    const bbox = turf.bbox(isochrone.toGeoJSON());
+    let grillePoints = [];
+
+    // Générer une grille de points à l'intérieur des bornes
+    for (let lon = bbox[0]; lon <= bbox[2]; lon += pas) {
+        for (let lat = bbox[1]; lat <= bbox[3]; lat += pas) {
+            let point = turf.point([lon, lat]);
+            // Vérifier si le point est à l'intérieur de l'isochrone
+            if (turf.booleanPointInPolygon(point, isochrone.toGeoJSON())) {
+                grillePoints.push([lat, lon]); // Stocker les points valides
+                
+                // Afficher le point sur la carte
+                // L.marker([lat, lon]).addTo(map);
+            }
         }
-        var coords = data.features[0].geometry.coordinates[0];
-        var latLngs = coords.map(coord => ([coord[1], coord[0]]));
+    }
 
-        // Utiliser Leaflet pour tracer l'isochrone sur la carte
-        currentIsochrone = L.polygon(latLngs, {
-            color: '#FF0000',
-            weight: 2,
-            opacity: 0.8,
-            fillColor: '#ffffff',
-            fillOpacity: 0.01
-        }).addTo(map);
-        map.setView([center.lat, center.lon], 13);
+    return grillePoints;
+}
 
-        const url = `https://api-adresse.data.gouv.fr/reverse/?lon=${center.lon}&lat=${center.lat}`;
+async function listerCommunesCouvertesParIsochrone(isochrone) {
+    let grillePoints = creerGrillePointsEtAfficherSurCarte(currentIsochrone, 0.005, carte);
+    for (let point of grillePoints) {
+        const url = `https://api-adresse.data.gouv.fr/reverse/?lon=${point[1]}&lat=${point[0]}`;
+        try {
+            const response = await fetch(url);
+            const dataReverse = await response.json();
+            if (dataReverse.features && dataReverse.features.length > 0) {
+                const codeINSEE = dataReverse.features[0].properties.citycode;
+                codesINSEE.add(codeINSEE);
+                // Ne pas retourner ici, continuez à collecter les codes INSEE
+            } else {
+                console.error('Communes couvertes isochrone: aucun résultat trouvé pour le point:', point[0], point[1]);
+                // Ne pas retourner ici, continuez à traiter les autres points
+            }
+        } catch (error) {
+            console.error('Erreur lors du géocodage du point:', 'lat', point[0], 'lon', point[1], error);
+            // Ne pas retourner ici, continuez à traiter les autres points
+        }
+    }
 
-        return fetch(url)
-            .then(response => response.json())
-            .then(data => {
-                if (data.features.length > 0) {
-                    const adresse = data.features[0];
-                    const codeINSEE = adresse.properties.citycode;
-                    alert(codeINSEE);
-                    return codeINSEE;
-                } else {
-                    throw new Error('Aucun résultat trouvé');
-                }
-            })
-            .catch(error => console.error('Erreur de géocodage:', error));
-
-    })
-    .catch(error => console.log('Erreur lors de la récupération des isochrones :', error));
+    // Retournez tous les codes INSEE collectés après avoir traité tous les points
+    return Array.from(codesINSEE);
 }
 
 
+async function chargerIsochroneEtListerCommunes() {
+    try {
+        await fetchIsochrone(carte, {"lat":data.lat,"lon":data.lon,"mode":"driving","time":10});
+        const codes = await listerCommunesCouvertesParIsochrone(currentIsochrone);
+        console.log("Codes INSEE des communes touchées:", codes);
+        await chargerEtablissements(codes);
+    } catch (error) {
+        console.error('Erreur:', error);
+    }
+}
 
-
-
-
-
-
-
-
-
-
-function estDansIsochrone(point, isochrone) {
-    return turf.inside(point, isochrone);
+async function chercherCoordonnees(adresse) {
+    const url = `https://api-adresse.data.gouv.fr/search/?q=${encodeURIComponent(adresse)}&limit=1`;
+    try {
+        const response = await fetch(url);
+        const data = await response.json();
+        if (data.features && data.features.length > 0) {
+            const coords = data.features[0].geometry.coordinates;
+            // Les coordonnées sont retournées sous la forme [longitude, latitude]
+            return { lon: coords[0], lat: coords[1] };
+        } else {
+            console.error('Aucun résultat trouvé pour l\'adresse:', adresse);
+            return null;
+        }
+    } catch (error) {
+        console.error('Erreur lors de la recherche des coordonnées:', error);
+        return null;
+    }
 }
 
 
-function chargerEtablissements() {
+async function chargerEtablissements(codesINSEE) {
+    const token = '354c9f77-e707-378c-b65c-b8b3e48d3da5'; // Utilisez votre token d'accès
+    console.log('codes INSEE ',codesINSEE);
+    for (const codeINSEE of codesINSEE) {
+        console.log('insee sirene :',codeINSEE);
+        const urlSirene = `https://api.insee.fr/entreprises/sirene/V3.11/siret?q=codeCommuneEtablissement:${codeINSEE} AND periode(activitePrincipaleEtablissement:86.21Z)&date=2024-01-01&nombre=100`;
 
-    if (0===0) {
-        const token = '2d4a57e2-c1de-3c24-981c-3309f92d139c'; // Utilisez votre token d'accès
-        const urlSirene = `https://api.insee.fr/entreprises/sirene/V3/siret?q=codeCommuneEtablissement:${codeCommune} AND periode(activitePrincipaleEtablissement:86.21)&date=2024-01-01`;
-        
-        // Fetch des données SIRENE
-        fetch(urlSirene, {
+        const response = await fetch(urlSirene, {
             method: 'GET',
             headers: {
                 'Authorization': `Bearer ${token}`,
                 'Content-Type': 'application/json'
             }
-        })
-        .then(response => response.json())
-        .then(data => {
-            console.log(data);
-            if (data.etablissements && data.etablissements.length > 0) {
-                data.etablissements.forEach((etablissement, index) => {
-                    // Construisez la variable infos avec les données de l'établissement
-                    const infos = `<strong> Unité légale </strong> <br>
-                    Sexe : ${etablissement.uniteLegale.sexeUniteLegale} <br> 
-                    NOM : ${etablissement.uniteLegale.nomUniteLegale} <br> 
-                    Prenom : ${etablissement.uniteLegale.prenom1UniteLegale} <br>
-                    Creation UL : ${etablissement.uniteLegale.dateCreationUniteLegale} <br>
-                    Activité principale UL : ${etablissement.uniteLegale.activitePrincipaleUniteLegale} <br>
-                    Tranche effectif : ${etablissement.uniteLegale.trancheEffectifsUniteLegale} <br>
-                    Caractère employeur : ${etablissement.uniteLegale.caractereEmployeurUniteLegale} <br> <br>
-                    <strong> Etablissement </strong> <br>
-                    Nom établissement : ${etablissement.periodesEtablissement[0].enseigne1Etablissement} <br>
-                    Activité principale etab : ${etablissement.periodesEtablissement[0].activitePrincipaleEtablissement} <br>
-                    Activité principale registre métier : ${etablissement.activitePrincipaleRegistreMetiersEtablissement} <br>
-                    Creation etab : ${etablissement.dateCreationEtablissement} <br>
-                    Caractère employeur etablissement : ${etablissement.periodesEtablissement[0].caractereEmployeurEtablissement} <br>
-                    Tranche effectif etablissement : ${etablissement.trancheEffectifsEtablissement} <br>
-                    Etablissement siège (boolean) : ${etablissement.etablissementSiege}`;
+        });
+        
+        if (!response.ok) {
+            throw new Error(`Erreur HTTP : ${response.status} ${codeINSEE}`);
+        }
+        
+        // Vérifiez le type de contenu avant de parser en JSON
+        const contentType = response.headers.get('content-type');
+        if (!contentType || !contentType.includes('application/json')) {
+            console.error("La réponse n'est pas du JSON :", await response.text());
+            return;
+        }
+        
+        const dataSirene = await response.json();
+            console.log(dataSirene);
 
-                
-                    const adresse = `${etablissement.adresseEtablissement.numeroVoieEtablissement} ${etablissement.adresseEtablissement.typeVoieEtablissement} ${etablissement.adresseEtablissement.libelleVoieEtablissement}, ${etablissement.adresseEtablissement.codePostalEtablissement} ${etablissement.adresseEtablissement.libelleCommuneEtablissement}`;
-                    
-                    setTimeout(() => {
-                        geocoderAdresse(adresse, infos);
-                    }, index * 0);
+            if (dataSirene.etablissements && dataSirene.etablissements.length > 0) {
+                dataSirene.etablissements.forEach(etablissement => {
+                    if (etablissement.adresseEtablissement.coordonneeLambertAbscisseEtablissement>0) {
+                        console.log(etablissement.adresseEtablissement.coordonneeLambertAbscisseEtablissement);
+                        const infos = `<strong>Établissement</strong><br>
+                        Nom : ${etablissement.uniteLegale.nomUniteLegale} ${etablissement.uniteLegale.prenom1UniteLegale}<br>
+                        Activité Principale : ${etablissement.periodesEtablissement[0].activitePrincipaleEtablissement}<br>
+                        Adresse : ${etablissement.adresseEtablissement.numeroVoieEtablissement} ${etablissement.adresseEtablissement.typeVoieEtablissement} ${etablissement.adresseEtablissement.libelleVoieEtablissement}, ${etablissement.adresseEtablissement.codePostalEtablissement} ${etablissement.adresseEtablissement.libelleCommuneEtablissement}<br>
+                        Adresse : ${etablissement.adresseEtablissement.coordonneeLambertAbscisseEtablissement}<br>
+                        SIREN : ${etablissement.siret}`;
+                        // Utiliser l'adresse géocodée si disponible ou géocoder l'adresse
+                        afficherSurCarte(
+                            etablissement.adresseEtablissement.coordonneeLambertAbscisseEtablissement,
+                            etablissement.adresseEtablissement.coordonneeLambertOrdonneeEtablissement,
+                            infos
+                        );
+                    }
+
                 });
-                
             }
-        })
-        .catch(error => console.error('Erreur :', error));
+        }
+    }
+
+function afficherSurCarte(lat, lon, infos) {
+    if (lat && lon) {
+        L.marker([lat, lon]).addTo(carte)
+            .bindPopup(infos)
+            .openPopup();
     } else {
-        alert('Veuillez entrer un code de commune.');
+        console.log("Coordonnées non disponibles pour l'établissement :", infos);
     }
 }
 
-function geocoderAdresse(adresse, infos) {
-    const url = `https://api-adresse.data.gouv.fr/search/?q=${encodeURIComponent(adresse)}&limit=1`;
-
-    fetch(url)
-    .then(response => response.json())
-    .then(data => {
-        if (data.features && data.features.length > 0) {
-            const lat = data.features[0].geometry.coordinates[1];
-            const lon = data.features[0].geometry.coordinates[0];
-            
-            // Créer un point GeoJSON pour l'établissement
-            var point = turf.point([lon, lat]);
-            
-            // Vérifier si le point est à l'intérieur de l'isochrone
-            if (currentIsochrone && estDansIsochrone(point, currentIsochrone.toGeoJSON())) {
-                afficherSurCarte(lat, lon, infos); // Seulement s'il est à l'intérieur de l'isochrone
-            } else {
-                console.log('Établissement hors de l\'isochrone:', adresse);
-            }
-        } else {
-            console.log('Aucun résultat trouvé pour l\'adresse:', adresse);
-        }
-    })
-    .catch(error => console.error('Erreur de géocodage:', error));
-}
-
-
-function afficherSurCarte(lat, lon, adresse) {
-    L.marker([lat, lon]).addTo(carte)
-      .bindPopup(adresse)
-      .openPopup();
-}
+chargerIsochroneEtListerCommunes();
